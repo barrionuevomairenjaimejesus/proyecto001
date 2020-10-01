8ab0c3ba6d21ccafc69b6d0886832b987dc4bb1d
jjbarrionuevo19@iespuntadelverde.es <jjbarrionuevo19@iespuntadelverde.es>
2020-10-01 13:34:32 +0200

    primer ejercicio

---
+++ src/ejercicio1.ts
@@ -0,0 +1,7 @@
+let nombre:string = "Nestor Kauil";
+let edad:number   = 25;
+const PERSONAJE: { nombre:string, edad:number } = {
+    nombre: nombre,
+    edad: edad
+}
+console.log(PERSONAJE);
---
+++ src/ejercicio2.ts
@@ -0,0 +1,6 @@
+let multiplicar = (a:number, b:number) => { 
+    let r = 4
+    let r2= a * b + r
+    return r2
+ }
+ console.log(multiplicar(3,4))
